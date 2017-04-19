bc5bb913e420eb3a532fbf5fa5e768d47f059741
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-04-19 11:26:42 -0700

    Update Lacinia dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 -- 19 Apr 2017
+
+Update dependency on Lacinia to latest, 0.15.0.
+
 ## 0.1.0 -- 19 Apr 2017
 
 First release.
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.walmartlabs/pedestal-lacinia "0.1.0"
+(defproject com.walmartlabs/pedestal-lacinia "0.1.1"
   :description "Pedestal infrastructure supporting Lacinia GraphQL"
   :url "https://github.com/walmartlabs/pedestal-lacinia"
   :license {:name "Apache Software License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.walmartlabs/lacinia "0.14.0"]
+                 [com.walmartlabs/lacinia "0.15.0"]
                  [com.fasterxml.jackson.core/jackson-core "2.5.3"]
                  [io.pedestal/pedestal.service "0.5.2"]
                  [io.pedestal/pedestal.jetty "0.5.2"]]
