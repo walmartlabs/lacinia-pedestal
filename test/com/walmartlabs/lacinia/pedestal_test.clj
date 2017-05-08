9770bd8705e8730c93d616c1819d8314d81a5be7
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-05-08 10:56:34 -0700

    Enable async query execution handler

--- test/com/walmartlabs/lacinia/pedestal_test.clj
+++ test/com/walmartlabs/lacinia/pedestal_test.clj
@@ -1,74 +1,13 @@
 (ns com.walmartlabs.lacinia.pedestal-test
   (:require
     [clojure.test :refer [deftest is use-fixtures]]
-    [clojure.java.io :as io]
-    [com.walmartlabs.lacinia.util :as util]
-    [com.walmartlabs.lacinia.schema :as schema]
-    [com.walmartlabs.lacinia.resolve :refer [resolve-as]]
     [com.walmartlabs.lacinia.pedestal :as lp]
-    [io.pedestal.http :as http]
     [clj-http.client :as client]
-    [cheshire.core :as cheshire]
-    [clojure.edn :as edn]
-    [clojure.string :as str]))
+    [clojure.string :as str]
+    [com.walmartlabs.lacinia.test-utils :refer [sample-schema-fixture
+                                                send-request]]))
 
-(defn ^:private resolve-echo
-  [context args _]
-  (let [{:keys [value error]} args
-        error-map (when error
-                    {:message "Forced error."
-                     :status error})
-        resolved-value {:value value
-                        :method (get-in context [:request :request-method])}]
-    (resolve-as resolved-value error-map)))
-
-(use-fixtures :once
-  (fn [f]
-    (let [schema (-> (io/resource "sample-schema.edn")
-                     slurp
-                     edn/read-string
-                     (util/attach-resolvers {:resolve-echo resolve-echo})
-                     schema/compile)
-          service (lp/pedestal-service schema {:graphiql true})]
-      (http/start service)
-      (try
-        (f)
-        (finally
-          (http/stop service))))))
-
-(defn ^:private get-url
-  [path]
-  (client/get (str "http://localhost:8888" path) {:throw-exceptions false}))
-
-(defn ^:private send-request
-  "Sends a GraphQL request to the server and returns the response."
-  ([query]
-   (send-request :get query))
-  ([method query]
-   (send-request method query nil))
-  ([method query vars]
-   (-> {:method method
-        :url "http://localhost:8888/graphql"
-        :throw-exceptions false}
-       (cond->
-         (= method :get)
-         (assoc-in [:query-params :query] query)
-
-         (= method :post)
-         (assoc-in [:headers "Content-Type"] "application/graphql")
-
-         ;; :post-bad is like :post, but without setting the content type
-         (#{:post :post-bad} method)
-         (assoc :body query
-                :method :post)
-
-         vars
-         (assoc-in [:query-params :variables] (cheshire/generate-string vars)))
-       client/request
-       (update :body #(try
-                        (cheshire/parse-string % true)
-                        (catch Exception t
-                          %))))))
+(use-fixtures :once (sample-schema-fixture {:graphiql true}))
 
 (deftest simple-get-query
   (let [response (send-request "{ echo(value: \"hello\") { value method }}")]
