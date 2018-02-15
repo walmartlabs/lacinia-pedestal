ec4c2231c12b36711d68300ecaae37e4de767a87
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-02-15 14:14:51 -0800

    Prep for 0.7.0-rc-1 release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.walmartlabs/lacinia-pedestal "0.7.0"
+(defproject com.walmartlabs/lacinia-pedestal "0.7.0-rc-1"
   :description "Pedestal infrastructure supporting Lacinia GraphQL"
   :url "https://github.com/walmartlabs/pedestal-lacinia"
   :license {:name "Apache Software License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.walmartlabs/lacinia "0.24.0"]
+                 [com.walmartlabs/lacinia "0.25.0-rc-1"]
                  [com.fasterxml.jackson.core/jackson-core "2.9.3"]
                  [io.pedestal/pedestal.service "0.5.3"]
                  [io.pedestal/pedestal.jetty "0.5.3"]
