caa13ff47fabb227a641948c2bf33474a20cc780
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-06-25 09:25:15 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,14 +6,12 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.walmartlabs/lacinia "0.28.0"]
                  [com.fasterxml.jackson.core/jackson-core "2.9.5"]
-                 [io.pedestal/pedestal.service "0.5.3"]
-                 [io.pedestal/pedestal.jetty "0.5.3"]]
+                 [io.pedestal/pedestal.service "0.5.4"]
+                 [io.pedestal/pedestal.jetty "0.5.4"]]
   :profiles
   {:dev {:dependencies [[clj-http "2.3.0"]
                         [com.walmartlabs/test-reporting "0.1.0"]
                         [expound "0.7.0"]
-                        ;; Overrides to match version of Jetty via Pedestal:
-                        [org.eclipse.jetty.websocket/websocket-client "9.4.0.v20161208"]
                         [stylefruits/gniazdo "1.0.1"
                          :exclusions [org.eclipse.jetty.websocket/websocket-client]]
 
