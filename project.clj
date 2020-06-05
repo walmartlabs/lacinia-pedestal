37e7913332a83361d913a3978f278bf1e7c919a9
Howard M. Lewis Ship <hlship@walmartlabs.com>
2020-06-04 17:45:02 -0700

    Update to version 0.14.0-alpha-2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.walmartlabs/lacinia-pedestal "0.14.0-alpha-1"
+(defproject com.walmartlabs/lacinia-pedestal "0.14.0-alpha-2"
   :description "Pedestal infrastructure supporting Lacinia GraphQL"
   :url "https://github.com/walmartlabs/pedestal-lacinia"
   :license {:name "Apache Software License 2.0"
