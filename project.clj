2fec16b2933acf4a5541960b3cc279fc9680f98b
Brett Adams <brettadams@fastmail.com>
2021-06-18 14:58:56 -0400

    remove test version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.walmartlabs/lacinia-pedestal "0.16-ba1"
+(defproject com.walmartlabs/lacinia-pedestal "0.16"
   :description "Pedestal infrastructure supporting Lacinia GraphQL"
   :url "https://github.com/walmartlabs/pedestal-lacinia"
   :license {:name "Apache Software License 2.0"
