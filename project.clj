830037a96d3f93ba843b3aeb8a64ecf24c7497cf
Howard Lewis Ship <h@lewisship.net>
2021-06-18 12:55:22 -0400

    Prep for 0.16
    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
-(defproject com.walmartlabs/lacinia-pedestal "0.15.0"
+(defproject com.walmartlabs/lacinia-pedestal "0.16"
   :description "Pedestal infrastructure supporting Lacinia GraphQL"
   :url "https://github.com/walmartlabs/pedestal-lacinia"
   :license {:name "Apache Software License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.walmartlabs/lacinia "0.38.0"]
-                 [com.fasterxml.jackson.core/jackson-core "2.12.1"]
+                 [com.fasterxml.jackson.core/jackson-core "2.12.3"]
                  [io.pedestal/pedestal.service "0.5.8"]
                  [io.pedestal/pedestal.jetty "0.5.8"]
-                 [org.clojure/data.json "1.0.0"]]
+                 [org.clojure/data.json "2.3.1"]]
   :profiles
   {:dev {:dependencies [[clj-http "3.11.0"]
                         [com.walmartlabs/test-reporting "1.0.0"]
                         [com.stuartsierra/component "1.0.0"]
-                        [expound "0.8.7"]
+                        [expound "0.8.9"]
                         [stylefruits/gniazdo "1.2.0"
                          :exclusions [org.eclipse.jetty.websocket/websocket-client]]
-                        [io.aviso/logging "0.3.2"]]}}
+                        [io.aviso/logging "1.0"]]}}
   :jvm-opts ["-Xmx500m"]
   :plugins [[lein-codox "0.10.7"]
             [test2junit "1.3.0"]
